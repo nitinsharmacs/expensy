bea76c81ccd90d7b35a44f57e31285a48b8e77a8
Nitin Sharma <48169425+nitinsharmacs@users.noreply.github.com>
2023-12-20 22:18:06 +0530

    Create service worker boilerplate

---
+++ ui/src/serviceWorkers/sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'CASHFLOW';
+
+self.addEventListener('install', (e) => {
+  console.log('Installing Service Worker');
+  e.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) =>
+        cache
+          .addAll(['/', '/index.html', 'assets/index-3b669906.js'])
+          .then(() => self.skipWaiting())
+      )
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  console.log('Activating Service Worker');
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  if (!navigator.onLine) {
+    return e.respondWith(
+      caches.match(e.request).then((response) => {
+        if (response) {
+          return response;
+        }
+      })
+    );
+  }
+
+  const req = e.request.clone();
+
+  return fetch(req).then((res) => {
+    if (res.status !== 200 || res.type !== 'basic') {
+      return res;
+    }
+
+    const cacheRes = res.clone();
+
+    caches.open(CACHE_NAME).then((cache) => cache.put(e.request, cacheRes));
+
+    return res;
+  });
+});
